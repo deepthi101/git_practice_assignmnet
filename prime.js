8ad735f291a200c11e437b025de7e40cdc4df160
deepthi <rdeepthi0213@gmail.com>
2022-09-21 15:20:46 +0530

    m1_code_fix

--- prime.js
+++ prime.js
@@ -1,14 +1,21 @@
-function Checkprime(num){
+function Check_prime(num){
   
 let factors=0;
-for(let i=1;i<=15;i++){
+for(let i=1;i<=num;i++){
   if(num%i==0){
     factors++;
   }
 }if(factors==2){
-  console.log(num,"is a prime no");
+  return true;
+}
+  return false;
+}
+let commit = Check_prime(24);
+if (commit == true){
+  console.log("is a prime no")
+​
+  
 }else{
-  console.log(num,"is not a prime no");
+  console.log("is not a prime no");
 }
-}
-Checkprime(15);
+
